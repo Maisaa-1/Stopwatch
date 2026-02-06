5499975afad799f9b9c7001817f3b951c5b71ec1
Maisaa Assy <mayassy894@gmail.com>
2026-02-07 00:06:32 +0200

    js logic fix

--- index.js
+++ index.js
@@ -1,82 +1,72 @@
-const startBtn = document.getElementById("btn-start")
-const stopBtn = document.getElementById("btn-stop")
-const resetBtn = document.getElementById("btn-reset")
+const startBtn = document.getElementById("btn-start");
+const stopBtn = document.getElementById("btn-stop");
+const resetBtn = document.getElementById("btn-reset");
 
-startBtn.addEventListener("click", startTimer)
-stopBtn.addEventListener("click", stopTimer)
-resetBtn.addEventListener("click", resetTimer)
+startBtn.addEventListener("click", startTimer);
+stopBtn.addEventListener("click", stopTimer);
+resetBtn.addEventListener("click", resetTimer);
 
-let interval
-let ms = 0
-let s = 0
-let min = 0
-let hr = 0
-const tensHTML = document.getElementById("tens")
-const secondsHTML = document.getElementById("second")
-const minutesHTML = document.getElementById("minute")
-const hoursHTML = document.getElementById("hours")
+let interval;
+let ms = 0;
+let s = 0;
+let min = 0;
+let hr = 0;
 
-
+const tensHTML = document.getElementById("tens");
+const secondsHTML = document.getElementById("second");
+const minutesHTML = document.getElementById("minute");
+const hoursHTML = document.getElementById("hours");
 
 function startTimer() {
-    interval = setInterval(increaseTime, 10)
+    // Prevent multiple intervals stacking
+    if (!interval) {
+        interval = setInterval(increaseTime, 10);
+    }
 }
 
 function increaseTime() {
-    ms++
-    if (ms < 10) {
-        tensHTML.innerText = "0" + ms
-    }
-    else if (ms >= 10 && ms <= 99) {
-        tensHTML.innerHTML = ms
+    ms++;
+    if (ms <= 99) {
+        tensHTML.innerText = ms < 10 ? "0" + ms : ms;
     } else {
-        s++
-        ms = 0
-        tensHTML.innerHTML = "0" + ms
-
+        s++;
+        ms = 0;
+        tensHTML.innerText = "00";
     }
-    if (s < 10) {
-        secondsHTML.innerText = "0" + s;
 
-    } else if (s >= 10 && s <= 60) {
-        secondsHTML.innerText = s
+    if (s <= 59) {
+        secondsHTML.innerText = s < 10 ? "0" + s : s;
     } else {
         min++;
-        s = 0
-        secondsHTML.innerText = "0" + s
+        s = 0;
+        secondsHTML.innerText = "00";
     }
-    if (min < 10) {
-        minutesHTML.innerText = "0" + min
-    } else if (min >= 10) {
-        minutesHTML.innerText = min
+
+    if (min <= 59) {
+        minutesHTML.innerText = min < 10 ? "0" + min : min;
     } else {
-        hr++
-        min = 0
-        minutesHTML.innerText = "0" + min
-    }
-    if (hr < 10) {
-        hoursHTML.innerText = "0" + hr
-    }
-    else if (hr>=10) { 
-        hoursHTML.innerText=hr 
+        hr++;
+        min = 0;
+        minutesHTML.innerText = "00";
     }
 
-
+    hoursHTML.innerText = hr < 10 ? "0" + hr : hr;
 }
 
-
 function stopTimer() {
- clearInterval(interval)
+    clearInterval(interval);
+    interval = null; // reset so startTimer can run again
 }
 
 function resetTimer() {
-    ms=0
-    s=0
-    min=0
-    hr=0
-    tensHTML.innerText = "0" + ms
-    secondsHTML.innerText="0" + s
-    minutesHTML.innerText="0" + min
-    hoursHTML.innerText="0" + hr
-    clearInterval(interval)
+    ms = 0;
+    s = 0;
+    min = 0;
+    hr = 0;
+    tensHTML.innerText = "00";
+    secondsHTML.innerText = "00";
+    minutesHTML.innerText = "00";
+    hoursHTML.innerText = "00";
+    clearInterval(interval);
+    interval = null;
 }
